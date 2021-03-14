726179b0df1829b60d893282efe5902a008e6963
JuanAngarita20 <80299132+JuanAngarita20@users.noreply.github.com>
2021-03-13 23:09:07 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,17 @@
 
 export const environment = {
   production: false
+  production: false,
+
+  firebaseConfig : {
+    apiKey: "AIzaSyAKuO8ExX3hnuV9EgxmikNbinnflHAgaQw",
+    authDomain: "foromultimedia-8511c.firebaseapp.com",
+    projectId: "foromultimedia-8511c",
+    storageBucket: "foromultimedia-8511c.appspot.com",
+    messagingSenderId: "558795473823",
+    appId: "1:558795473823:web:ac966b71e0622c972533aa"
+  }
+  
 };
 
 /*
