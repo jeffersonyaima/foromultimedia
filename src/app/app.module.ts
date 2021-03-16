66cfaedea38fb93ef2d04c08f153dd3e4475c7df
JuanAngarita20 <80299132+JuanAngarita20@users.noreply.github.com>
2021-03-16 17:54:07 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
+import { environment } from 'src/environments/environment';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import {ReactiveFormsModule} from '@angular/forms';
+
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireAuthModule} from '@angular/fire/auth';
 
 
 
@@ -11,7 +16,10 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
   declarations: [AppComponent, NavbarComponent],
   imports: [
     AppRoutingModule,
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
