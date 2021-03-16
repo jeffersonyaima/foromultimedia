0a5d6614f41e4937315aa9a3d5966c95f75cba1f
JuanAngarita20 <80299132+JuanAngarita20@users.noreply.github.com>
2021-03-16 17:52:04 -0500

    Update navbar.component.ts

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import {AuthService} from './../../auth/services/auth.service'
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  providers: [AuthService],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent{
 
-  constructor() { }
+  public user$: Observable<any> = this.authSvc.afAuth.user;
+  
+  constructor(private authSvc: AuthService, private router:Router) { }
 
-  ngOnInit(): void {
+  async onLogout(){
+    try{
+      await this.authSvc.logout();
+      this.router.navigate(['/login']);
+    }
+    catch(error){
+      console.log(error);
+    }
   }
 
 }
