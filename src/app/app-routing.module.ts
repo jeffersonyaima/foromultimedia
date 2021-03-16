2e395f357d2ec8e9b24242848a13b1e35acb5aeb
JuanAngarita20 <80299132+JuanAngarita20@users.noreply.github.com>
2021-03-16 00:15:18 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {Routes, RouterModule} from '@angular/router';
 
-const routes:Routes=[];
+const routes:Routes=[
+  { path: '', redirectTo: '/home', pathMatch: 'full',},
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, 
+  { path: 'login', loadChildren: () => import('./auth/login/login.module').then(m => m.LoginModule) }, 
+  { path: 'register', loadChildren: () => import('./auth/register/register.module').then(m => m.RegisterModule) }];
 
 
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes),
-    CommonModule
-  ]
+    RouterModule.forRoot(routes),CommonModule],
+    exports:[RouterModule]
 })
 export class AppRoutingModule { }
