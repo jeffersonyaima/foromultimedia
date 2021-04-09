01932d4ddcd6a1a4cbd9e5815200a1f52f5870ed
JuanAngarita20 <80299132+JuanAngarita20@users.noreply.github.com>
2021-04-08 20:27:16 -0500

    Update register.component.ts

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,35 +1,61 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormControl} from '@angular/forms'
+import {FormGroup, FormControl, FormBuilder} from '@angular/forms'
 import { Router } from '@angular/router';
-import {AuthService} from './../services/auth.service'
+import {AuthService} from './../services/auth.service';
+import { FirestoreService } from 'src/app/services/firestore/firestore.service';
+
 
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
-  providers:[AuthService],
+  providers:[FirestoreService]
 })
 export class RegisterComponent implements OnInit {
-  registerForm= new FormGroup({
-    email : new FormControl(''),
-    password: new FormControl(''),
+
+
+  public registerForm= new FormGroup({
+    /*email : new FormControl(''),
+    password: new FormControl(''),*/
   })
 
-  constructor(private authSvc:AuthService, private router: Router) { }
+  
+  constructor(private fb: FormBuilder, private authSvc:AuthService, private router: Router,private firestoreService: FirestoreService) {} 
 
-  ngOnInit(): void {}
 
- async  onRegister(){
+  private initForm():void{
+    this.registerForm = this.fb.group({
+      email:[''],
+      name:[''],
+      apellido:[''],
+      nombreusuario:[''],
+      password:[''],
+    });
+  }
+
+  
+ ngOnInit(): void  {
+  this.initForm();
+ }
+
+
+ onRegisNuevo(){
+  console.log('registro',this.registerForm.value);
+   const formValue = this.registerForm.value;
+   this.firestoreService.guardarusuario(formValue);
+   this.firestoreService.registrarUsuario(formValue);
+ }
+
+
+ onRegister(){
+
    const {email, password}= this.registerForm.value;
-   try{
-    const user = this.authSvc.register(email, password);
+   const user = this.authSvc.register(email, password);
+
     if(user){
       this.router.navigate(['/home']);
     }
-   }
-   catch{}
     
   }
-
 }
