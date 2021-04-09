8cacc4a4b416ac692c880899ada91ea2a36641e1
JuanAngarita20 <80299132+JuanAngarita20@users.noreply.github.com>
2021-04-08 23:43:28 -0500

    Update menu.component.ts

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import {AuthService} from '.././auth/services/auth.service';
+
 declare var jQuery: any;
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
+  providers: [AuthService],
 })
 export class MenuComponent implements OnInit {
 
   habilitar: boolean = true;
 
-  constructor() { }
+  public user$: Observable<any> = this.authSvc.afAuth.user;
 
-
+  
+  constructor( private authSvc: AuthService, private router:Router) { }
 
 
   ngOnInit(){
