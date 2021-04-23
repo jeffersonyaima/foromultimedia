dbc23381037a6fe81ca724eefcb36d2ed76e53b0
JuanAngarita20 <80299132+JuanAngarita20@users.noreply.github.com>
2021-04-23 15:29:44 -0500

    Cambios Foro

--- src/app/secciones/foro/foro.component.ts
+++ src/app/secciones/foro/foro.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FirestoreService } from 'src/app/services/firestore/firestore.service';
+import { AngularFirestore, AngularFirestoreCollection, fromDocRef} from '@angular/fire/firestore';
+import {AuthService} from '../../auth/services/auth.service';
 declare var jQuery: any;
+
 
 @Component({
   selector: 'app-foro',
@@ -8,7 +13,32 @@ declare var jQuery: any;
 })
 export class ForoComponent implements OnInit {
 
-  constructor() { }
+  validar:boolean=false;
+  own_q:boolean=true;
+
+
+  constructor(private authSvc: AuthService, private readonly firestore: AngularFirestore) { 
+    this.authSvc.afAuth.user.forEach((dato)=>{  /* en Datos se guarda informacion del usuario loogeado */
+      console.log(dato); /*Dato es array que guarda informacion*/ 
+      if(dato) {
+        console.log(dato.email); /*dato.email guarda valor "email" guardado en array dato*/ 
+        let correo = dato.email; /* Se guarda en correo valor de dato.email*/ 
+
+     /*MISMA FUNCION leerdatos DE FIRESTORE SERVICE, SE PUSO AQUI PORQUE ES NECESARIO*/ 
+      this.firestore.collection('userlist', ref=>ref.where("email",'==',correo)).get().subscribe((resultado)=>{
+        resultado.docs.forEach((item)=>{
+          let usuario :any = item.data();
+          let ad:any = usuario.admin;
+          console.log(ad);
+          this.validar=ad;
+          console.log(this.validar);
+        })
+      })
+       
+      }
+      else console.log('NO ESTA LOGEADO');
+    })
+  }
 
 
   ngOnInit(): void {
