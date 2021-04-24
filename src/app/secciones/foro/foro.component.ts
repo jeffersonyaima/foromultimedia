6b049cc7ec1d6fee20f4c020edb2ff7bb32541d9
jeffersonyaima <geserfon@hotmail.com>
2021-04-24 01:16:06 -0500

    esqueleto foro
    prueba 1

--- src/app/secciones/foro/foro.component.ts
+++ src/app/secciones/foro/foro.component.ts
@@ -15,16 +15,17 @@ export class ForoComponent implements OnInit {
 
   validar:boolean=false;
   own_q:boolean=true;
+  own_a:boolean=true;
 
 
-  constructor(private authSvc: AuthService, private readonly firestore: AngularFirestore) { 
+  constructor(private authSvc: AuthService, private readonly firestore: AngularFirestore) {
     this.authSvc.afAuth.user.forEach((dato)=>{  /* en Datos se guarda informacion del usuario loogeado */
-      console.log(dato); /*Dato es array que guarda informacion*/ 
+      console.log(dato); /*Dato es array que guarda informacion*/
       if(dato) {
-        console.log(dato.email); /*dato.email guarda valor "email" guardado en array dato*/ 
-        let correo = dato.email; /* Se guarda en correo valor de dato.email*/ 
+        console.log(dato.email); /*dato.email guarda valor "email" guardado en array dato*/
+        let correo = dato.email; /* Se guarda en correo valor de dato.email*/
 
-     /*MISMA FUNCION leerdatos DE FIRESTORE SERVICE, SE PUSO AQUI PORQUE ES NECESARIO*/ 
+     /*MISMA FUNCION leerdatos DE FIRESTORE SERVICE, SE PUSO AQUI PORQUE ES NECESARIO*/
       this.firestore.collection('userlist', ref=>ref.where("email",'==',correo)).get().subscribe((resultado)=>{
         resultado.docs.forEach((item)=>{
           let usuario :any = item.data();
@@ -34,7 +35,7 @@ export class ForoComponent implements OnInit {
           console.log(this.validar);
         })
       })
-       
+
       }
       else console.log('NO ESTA LOGEADO');
     })
