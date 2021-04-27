02bd58e2b46e6cc0c2f2cbaadadb1f8eb54d8e60
danielsilva14 <est.daniela.silva@unimilitar.edu.co>
2021-04-26 19:29:34 -0500

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 declare var jQuery: any;
 
-import { gsap, Elastic } from 'gsap'
+
 
 @Component({
   selector: 'app-home',
@@ -30,55 +30,7 @@ export class HomeComponent implements OnInit {
 
     (function ($) {
 
-      $(document).ready(function(){
-        $("#logo3d").hover(function(){$("#logo3d"),
-        gsap.from('#logo3d', { duration: 2, ease: "elastic.out(2, 0.1)", y: -10 }); }, 
-        function(){$("logo3d"),
-        gsap.set('logo3d', { duration: 2, ease: "elastic.out(2, 0.1)", y: -10 });
-        });});
-
-      $(document).ready(function(){
-        $("#logoguiones").hover(function(){$("#logoguiones"),
-        gsap.from('#logoguiones', { duration: 2, ease: "elastic.out(2, 0.1)", y: -10 }); }, 
-        function(){$("logoguiones"),
-        gsap.set('logoguiones', { duration: 2, ease: "elastic.out(2, 0.1)", y: -10 });
-        });});
-
-      $(document).ready(function(){
-          $("#logo2d").hover(function(){$("#logo2d"),
-          gsap.from('#logo2d', { duration: 3, ease: "elastic.out(2, 0.1)", y: -10 }); }, 
-          function(){$("#logo2d"),
-          gsap.set('#logo2d', { duration: 3, ease: "elastic.out(2, 0.1)", y: -10 });
-          });});  
-    
-       $(document).ready(function(){
-          $("#logoaudio").hover(function(){$("#logoaudio"),
-          gsap.from('#logoaudio', { duration: 2, ease: "elastic.out(2, 0.1)", y: -10 }); }, 
-          function(){$("#logoaudio"),
-          gsap.set('#logoaudio', { duration: 2, ease: "elastic.out(2, 0.1)", y: -10 });
-          });});  
-    
-       $(document).ready(function(){
-          $("#logoprogramacion").hover(function(){$("#logoprogramacion"),
-          gsap.from('#logoprogramacion', { duration: 2, ease: "elastic.out(2, 0.1)", y: -10 }); }, 
-          function(){$("#logoprogramacion"),
-          gsap.set('#logoprogramacion', { duration: 2, ease: "elastic.out(2, 0.1)", y: -10 });
-          });});  
-    
-       $(document).ready(function(){
-          $("#logorealidad").hover(function(){$("#logorealidad"),
-          gsap.from('#logorealidad', { duration: 2, ease: "elastic.out(2, 0.1)", y: -10 }); }, 
-          function(){$("#logorealidad"),
-          gsap.set('#logorealidad', { duration: 2, ease: "elastic.out(2, 0.1)", y: -10 });
-          });});  
-
-       $(document).ready(function(){
-          $("#mostrar").click(function(){
-          $('.target').show("swing");
-          });
-          $("#ocultar").click(function(){
-          $('.target').hide("linear");
-          });});
+      
 
     })(jQuery);
 
