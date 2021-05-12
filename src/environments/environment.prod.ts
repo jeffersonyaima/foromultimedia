c88e11fd8bb4fb5cae4ffb35d58171b47acaf66e
JuanAngarita20 <80299132+JuanAngarita20@users.noreply.github.com>
2021-05-11 23:01:32 -0500

    Hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  
+  firebaseConfig : {
+    apiKey: "AIzaSyAKuO8ExX3hnuV9EgxmikNbinnflHAgaQw",
+    authDomain: "foromultimedia-8511c.firebaseapp.com",
+    projectId: "foromultimedia-8511c",
+    storageBucket:  "foromultimedia-8511c.appspot.com",
+    messagingSenderId: "558795473823",
+    appId: "1:558795473823:web:ac966b71e0622c972533aa"
+  }
+
 };
